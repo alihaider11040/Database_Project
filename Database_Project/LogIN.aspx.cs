181ac784e046dd15ac3a350c9e501261dc79d989
Auroob366 <l190906@lhr.nu.edu.pk>
2021-12-20 14:05:08 +0500

    Connection in progress

--- Database_Project/LogIN.aspx.cs
+++ Database_Project/LogIN.aspx.cs
@@ -4,18 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Database_Project
 {
     public partial class LogIN : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection(@"");
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (con.State == ConnectionState.Open) {
+                con.Close();
+            }
+            con.Open();
         }
         protected void loginClick(object sender, EventArgs e) {
 
-            string username = this.user.Text;
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into LOGINS ";
         
         }
 
